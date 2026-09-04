b06f040279c6df5b5e78b64f561ef03951a478da
Ryan E Davis <redavi19@asu.edu>
2026-09-04 12:14:39 -0400

    Apply neutral paralegal demo branding

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CandiceParalegalLanding from "./App.jsx";
+import ParalegalLanding from "./App.jsx";
+import { removeLegacyName } from "./legacyNameCleanup.js";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <CandiceParalegalLanding />
+    <ParalegalLanding />
   </React.StrictMode>
 );
+
+removeLegacyName();
